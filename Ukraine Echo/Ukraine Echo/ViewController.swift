bbb5d0b06c8bd603d57355c86a48f568a25f5a44
drulik44 <sahanzxi984325qx@gmail.com>
2025-07-21 16:42:54 +0300

    remove storyboard and based settings project

---
+++ Ukraine Echo/Ukraine Echo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ukraine Echo
+//
+//  Created by Руслан Жидких on 21.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+    }
+
+
+}
+
