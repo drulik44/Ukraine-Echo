72d353864d84d011317dfc8a3e310016f6db1011
drulik44 <sahanzxi984325qx@gmail.com>
2025-08-11 19:41:15 +0300

    added library and colors

---
+++ Ukraine Echo/Ukraine Echo/Resources/AppColors.swift
@@ -0,0 +1,17 @@
+//
+//  AppColors.swift
+//  Ukraine Echo
+//
+//  Created by Руслан Жидких on 11.08.2025.
+//
+
+import Foundation
+import UIKit
+
+enum AppColors {
+    static let backgroundColor = UIColor(hex: 0x131313)
+    static let yellow = UIColor(hex: 0xFFD00F)
+    static let accentBlue = UIColor(hex: 0x4464E8)
+    static let gray = UIColor(hex: 0x333333)
+    static let blue = UIColor(hex: 0xD6EDFF)
+}
